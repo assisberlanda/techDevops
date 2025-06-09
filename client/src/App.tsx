bff0fc2bc6281a5cdd25a5cd12693af1755c7f5d
Assis Berlanda de Medeiros <berlanda.medeiros@gmail.com>
2025-06-09 15:01:20 +0100

    Atualizaçao

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router as WouterRouter } from "wouter";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
@@ -9,14 +9,17 @@ import { Navbar } from "@/components/Navbar";
 import { BackToTop } from "@/components/BackToTop";
 import { ThemeProvider } from "@/hooks/use-theme";
 import { LanguageProvider } from "@/hooks/use-language";
+import customHashHistory from "../utils/hashLocation.ts"; // ✅ ajuste para hash routing
 
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/admin" component={Admin} />
-      <Route component={NotFound} />
-    </Switch>
+    <WouterRouter hook={() => customHashHistory}>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/admin" component={Admin} />
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
   );
 }
 
