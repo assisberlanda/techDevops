c27cbebbf7ded5e7049fe5f4701121abfbc04a73
Assis Berlanda de Medeiros <berlanda.medeiros@gmail.com>
2025-06-13 13:04:56 +0100

    Ultima correção do modo escuro na branch dev

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,46 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router as WouterRouter } from "wouter";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 import Admin from "@/pages/Admin";
+import PrivacyPolicy from "@/pages/PrivacyPolicy";
+import TermsOfUse from "@/pages/TermsOfUse";
 import { Navbar } from "@/components/Navbar";
 import { BackToTop } from "@/components/BackToTop";
-import { ThemeProvider } from "@/hooks/use-theme";
 import { LanguageProvider } from "@/hooks/use-language";
+import hashLocationHook from 'wouter/use-hash-location';
 
-function Router() {
+// ---> REMOVIDO: A importação do ThemeProvider daqui.
+// import { ThemeProvider } from "@/hooks/use-theme";
+
+function AppRouter() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/admin" component={Admin} />
-      <Route component={NotFound} />
-    </Switch>
+    <WouterRouter hook={hashLocationHook}>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/admin" component={Admin} />
+        <Route path="/privacy-policy" component={PrivacyPolicy} />
+        <Route path="/terms-of-use" component={TermsOfUse} />
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
   );
 }
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark">
-      <LanguageProvider>
-        <QueryClientProvider client={queryClient}>
-          <Navbar />
-          <Router />
-          <BackToTop />
-          <Toaster />
-        </QueryClientProvider>
-      </LanguageProvider>
-    </ThemeProvider>
+    // ---> REMOVIDO: O ThemeProvider que estava aqui.
+    <LanguageProvider>
+      <QueryClientProvider client={queryClient}>
+        <Navbar />
+        <AppRouter />
+        <BackToTop />
+        <Toaster />
+      </QueryClientProvider>
+    </LanguageProvider>
+    // ---> REMOVIDO: O ThemeProvider que estava aqui.
   );
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,10 +5,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 // Importa o ThemeProvider do local correto
-import { ThemeProvider } from './components/theme-provider.tsx'
+import { ThemeProvider } from './components/theme-provider' // <- Corrigido para remover o .tsx da importação
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    {/* Este é o único ThemeProvider que o app deve ter */}
     <ThemeProvider defaultTheme="dark" storageKey="ui-theme">
       <App />
     </ThemeProvider>
