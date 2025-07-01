3e35cd1834b06084edd85ded6d21886e218117fd
Assis Berlanda de Medeiros <berlanda.medeiros@gmail.com>
2025-07-01 12:36:07 +0100

    Corrigido rota main.tsx usando gemini-cli

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 // Importa o ThemeProvider do local correto
-import { ThemeProvider } from './components/theme-provider.tsx'
+import { ThemeProvider } from './hooks/use-theme.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
